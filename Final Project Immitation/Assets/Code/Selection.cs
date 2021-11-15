6c9c9672f0dccfd14b0161dbc1272d19218136ae
Keistie <mh5448@nyu.edu>
2021-11-15 01:58:28 -0500

    Use Switch and add equip

--- Final Project Immitation/Assets/Code/Selection.cs
+++ Final Project Immitation/Assets/Code/Selection.cs
@@ -6,6 +6,7 @@ public class Selection : MonoBehaviour
 {
     private Transform trans;
     public int chooseStage = 0;
+    public int equip = 0;
 
     void Awake(){
         trans = GetComponent<Transform>();
@@ -24,7 +25,7 @@ public class Selection : MonoBehaviour
         }
     }
 
-    public void PostitionChange(){
+   /* public void PostitionChange(){
         if(chooseStage == 0){
             trans.position = new Vector3 (-8, 4);
         }
@@ -41,7 +42,39 @@ public class Selection : MonoBehaviour
             trans.position = new Vector3 (5, 4);
         }
     }
+*/
+    void Actions (){
+        switch(chooseStage){
+            case 0:
+                trans.position = new Vector3 (-8, 4);
+                break;
+            case 1:
+                if(Input.GetKeyDown(KeyCode.Space)){
+                    equip = 1;
+                }
+                if(Input.GetKeyDown(KeyCode.P)) {
+                    equip = 0;
+                }
+                if(equip == 0){
+                    trans.position = new Vector3 (-5, 4);   
+                }
+                else if(equip == 1){
+                    trans.position = new Vector3(-6, -2);
+                }
+                break;
+            case 2:
+                trans.position = new Vector3 (-2, 4);
+                break;
+            case 3:
+                trans.position = new Vector3 (2, 4);
+                break;
+            case 4:
+                trans.position = new Vector3 (5, 4);
+                break;
 
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -52,7 +85,7 @@ public class Selection : MonoBehaviour
            Back();
        }     
 
-       PostitionChange();
+       Actions();
         
     }
 }
